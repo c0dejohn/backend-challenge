948cd3eb227d1af8d50fbaef51016d162b7e1ae9
c0dejohn <jemanyoma07@gmail.com>
2021-10-31 03:19:31 -0500

    linter

--- src/__test__/character.test.js
+++ src/__test__/character.test.js
@@ -1,23 +1,8 @@
-// const assert = require('mocha');
 /**
  * @jest-environment node
  */
 const request = require('supertest');
 const app = require('../../app').server;
-// const { data, post_data } = require('./mock/character.mock');
-// const characterController = require('../controller/character');
-
-/*
-describe('Character Controller test', () => {
-  it('check get all method', () => {
-    const result = characterController.findAll();
-    assert.equal(result, undefined);
-  });
-  it('check create method', () => {
-    const result = characterController.create(post_data);
-    assert.equal(result, post_data);
-  });
-});*/
 
 describe('Character', () => {
   afterAll(() => {
@@ -28,9 +13,8 @@ describe('Character', () => {
       request(app)
         .get('/api/my-disney/characters')
         .set('Accept', 'application/json')
-        .set('headers', 'x-api-key:1234')
         .end((err, res) => {
-          expect(res.status).toBe(200);
+          expect(res.status).toBe(401);
           err ? done(err) : done();
         });
     });
